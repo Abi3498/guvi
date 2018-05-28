d320bccf1370610398cd78ed68b3528c5895b56a
Abi3498 <36623738+Abi3498@users.noreply.github.com>
2018-05-28 18:47:49 +0530

    Add files via upload

---
+++ big.py
@@ -0,0 +1,19 @@
+a=input("Enter a")
+
+b=input("Enter b")
+
+c=input("Enter c")
+
+if(a>b) and (a>c):
+
+    lar=a
+
+elif(b>a) and (b>c):
+
+    lar=b
+
+else:
+
+    lar=c
+
+print("Greatest value is:",lar)
