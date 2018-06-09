ad2b0ca0a1fc96f6c881e41aa57af723762f3de2
Abi3498 <36623738+Abi3498@users.noreply.github.com>
2018-06-09 23:12:00 +0530

    Add files via upload

---
+++ pali.py
@@ -0,0 +1,21 @@
+num=int(input("Enter num value"))
+
+rev=0
+
+while(num>0):
+
+ rem=num%10
+
+ rev=(rev*10)+rem
+
+ num=num/10
+
+print ("reverse value:",rev)
+ 
+if(num==rev):
+
+  print ("Yes")
+
+if(num!=rev):
+
+ print ("No")
