138b50a00d3d5b45e6246302147e0e63d3993497
Abi3498 <36623738+Abi3498@users.noreply.github.com>
2018-06-09 23:26:05 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,20 @@
+count=0
+
+num=int(input("Enter num value"))
+
+for i in range(1,num+1):
+
+ if(num%i==0):
+
+  count=count+1
+
+print (count)
+
+if(count==2):
+
+ print ("prime")
+
+else:
+
+ print ("No")
+ 
