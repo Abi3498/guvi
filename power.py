a18ba2b41956d60bde0c1d8e56f11900fac11d20
Abi3498 <36623738+Abi3498@users.noreply.github.com>
2018-06-09 21:50:55 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,16 @@
+base=int(input("enter base:"))
+
+exp=int(input("enter exp"))
+
+def power(base,exp):
+
+ if(exp==1):
+
+  return(base)
+
+ if(exp!=1):
+
+  return(base*power(base,exp-1))
+
+print("power of a number:",power(base,exp))
+	
